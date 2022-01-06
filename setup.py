c154a50323083f48509bfd6552b649b95bf9ea27
Valentin Laurent <valentin.laurent.fr@gmail.com>
2022-01-06 23:58:12 +0100

    Finished changing repo name. Removed script installation (unsused for now).

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open('requirements.txt') as f:
     content = f.readlines()
 requirements = [x.strip() for x in content if 'git+' not in x]
 
-setup(name='evaluating-student-writing',
+setup(name='evalstudent',
       version="1.0",
       description="Project Description",
       packages=find_packages(),
       install_requires=requirements,
-      test_suite='tests',
+      # test_suite='tests',
       # include_package_data: to install data from MANIFEST.in
       include_package_data=True,
-      scripts=['scripts/evaluating-student-writing-run'],
+      # scripts=['scripts/evaluating-student-writing-run'],
       zip_safe=False)
